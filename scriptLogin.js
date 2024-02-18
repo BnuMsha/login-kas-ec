bb80c74004e86e047caf1f1544a2ff149042d110
BnuMsha <tianumaesha@gmail.com>
2024-02-18 18:44:31 +0700

    for english club

---
+++ scriptLogin.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var video = document.getElementById('background-video');
+
+    // Mengatur kecepatan video menjadi setengah dari kecepatan aslinya
+    video.playbackRate = 0.65;
+});
